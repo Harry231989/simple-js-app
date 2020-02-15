6f8513644aa6e1ecd10b8247e0f05ffff06bdf77
Harry231989 <59657318+Harry231989@users.noreply.github.com>
2020-02-14 18:04:19 -0800

    added new objects
    object added with same key-value pair

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,39 @@ var repositoryArray = [
   {age:9},
   {arm:'big'},
   ];
+
+var mixedArray = [
+  {name:"Bulbasaur"},
+  {height:7},
+  {types:'grass' ,'poison'},
+  {age:9},
+  {arm:'big'},
+];
+
+var myPokeArray = [
+  {name:"Bulbasaur"},
+  {height:7},
+  {types:'grass' ,'poison'},
+  {age:9},
+  {arm:'big'},
+];
+
+var repoArray = [
+  {name:"Bulbasaur"},
+  {height:7},
+  {types:'grass' ,'poison'},
+  {age:9},
+  {arm:'big'},
+
+];
+
+var newArray = [
+  {name:"Bulbasaur"},
+  {height:7},
+  {types:'grass' ,'poison'},
+  {age:9},
+  {arm:'big'},
+
+];
+
+
