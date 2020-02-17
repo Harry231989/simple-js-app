ba4e07dad24c1043cd119089258dcc09719647a2
Harry231989 <59657318+Harry231989@users.noreply.github.com>
2020-02-17 02:36:01 -0800

    Updated Array
    needed to remove some of the brackets ..

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,54 @@
+/*This is a long comment .it explains several objects to the array that represents Pokemon*/
+
 var repositoryArray = [
-  {name:"Bulbasaur"},
-  {height:7},
-  {types:'grass' , 'poison'},
-  {age:9},
-  {arm:'big'},
-  ];
+  {
+  	name:"Bulbasaur",
+  	height:7,
+  	types:'grass' , 'poison',
+  	age:9,
+  	arm:'big',
+  }
+];
 
 var mixedArray = [
-  {name:"Bulbasaur"},
-  {height:7},
-  {types:'grass' ,'poison'},
-  {age:9},
-  {arm:'big'},
+  {
+  	name:"Bulbasaur",
+  	height:7,
+  	types:'grass' , 'poison',
+  	age:9,
+  	arm:'big',
+  }
+
 ];
 
 var myPokeArray = [
-  {name:"Bulbasaur"},
-  {height:7},
-  {types:'grass' ,'poison'},
-  {age:9},
-  {arm:'big'},
+  {
+  	name:"Bulbasaur",
+  	height:7,
+  	types:'grass' ,'poison',
+  	age:9,
+  	arm:'big',
+  }
 ];
 
 var repoArray = [
-  {name:"Bulbasaur"},
-  {height:7},
-  {types:'grass' ,'poison'},
-  {age:9},
-  {arm:'big'},
+  {
+  	name:"Bulbasaur" ,
+  	height:7,
+  	types:'grass' , 'poison',
+  	age:9,
+  	arm:'big',
+   }
 
 ];
 
 var newArray = [
-  {name:"Bulbasaur"},
-  {height:7},
-  {types:'grass' ,'poison'},
-  {age:9},
-  {arm:'big'},
+  {
+  	name:"Bulbasaur",
+  	height:7,
+  	types:'grass' , 'poison',
+  	age:9,
+  	arm:'big',
+   }
 
 ];
-
-
