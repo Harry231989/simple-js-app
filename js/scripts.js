01523aefd128d64f7741ea2a38d6b0432efaa897
Harry231989 <59657318+Harry231989@users.noreply.github.com>
2020-02-24 08:17:40 -0800

    Changed coded
    added new syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 /*This is a long comment .it explains several objects to the array that represents Pokemon*/
 
+
 var repositoryArray = [
   {
-  	name:"Bulbasaur",
+  	name:'Bulbasaur',
   	height:7,
-  	types:'grass' , 'poison',
+  	types:['grass' , 'poison'],
   	age:9,
   	arm:'big',
   },
+
   {
   	name:'Charizard',
   	height:1.7,
   	types:['eletricity'],
 
   },
+
   {
   	name:'Squirtle',
   	height:0.5,
-  	types:['grass' , 'poison']
+  	types:['grass' , 'poison'],
   	age:9,
   	arm:'big',
-  }
+  },
 ];
-
-
+document.write('repositoryArray');
 for (var i = 1; i <=3;i++) {
 	console.log(i);
+	
 };	
-   document.write(Pokemon);
+  
 
 var name = 'Bulbasaur';
 var height = 7;
@@ -38,8 +41,14 @@ var name = 'Charizard';
 var height = 1.7;
 document.write('Charizard(height:1.7)'  + "Wow, that's big!");
 
+
 var name = 'Squirtle';
 var height = 0.5;
 document.write('Squirtle(height:0.5)');
+
+
+
+
+
 
 
