f8e46a3506ac5050ad622d3bc91861ecd9a62d42
Harry231989 <59657318+Harry231989@users.noreply.github.com>
2020-03-04 15:54:31 -0800

    code added
    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 /*This is a long comment .it explains several objects to the array that represents Pokemon*/
-
-
-var repositoryArray = [
+ var pokemonRepository = (function  () {
+  	 var repositoryArray = [
   {
   	name:'Bulbasaur',
   	height:7,
@@ -25,41 +24,54 @@ var repositoryArray = [
   	arm:'big',
   },
 ];
-/*
 
+function add(pokemonRepository.add(item) {
+	repositoryArray.push(pokemonRepository);
+}
+
+function getAll(pokemonRepository.getAll() {
+	return pokemonRepository;
+}
+
+return {
+	add:  add,
+	getAll:  getAll
+};
+
+})();
+
+
+
+ 
+
+
+
+
+
+
+/*
+  
 var repositoryArray = ['Bulbasaur'  +  'Charizard'  +  'Squirtle'];
 var arrayLength = repositoryArray.length;
 for (var i = 0; i < arrayLength;i++) {
-    console.log(repositoryArray[i]);
+	console.log(repositoryArray[i]);
     document.write("<p>"  +  'Bulbasaur(height:7)' + 'grass','poison'  +  9  +  'big'  + "</p>");
     document.write("<p>"  +  'Charizard(height:1.7)-Wow thats big'  + 'grass','poison'  +  "</p>");
     document.write("<p>"  +  'Squirtle(height:0.5)'    +   'grass','poison'  +  9  +  'big'  + "</p>")
 
 
-
-};	
-
+};
 */
 
 
- repositoryArray.forEach(function(repositoryArray)  {
+ repositoryArray.forEach(function(pokemonRepository)  {
  	document.write(repositoryArray.name  +  '<br>')
-  document.write(repositoryArray.height  +  '<br>')
+ 	document.write(repositoryArray.height  +  '<br>')
  	document.write(repositoryArray.types  +  '<br>')
  	document.write(repositoryArray.age  +  '<br>')
  	document.write(repositoryArray.arm  +  '<br>')
-   
+  
  });
-
-
-
-
-  
-  
-
-
-
-
 
 
 
